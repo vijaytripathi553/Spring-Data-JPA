878b5bf9a8e4b35f874ce856a6a246b93d6ceea2
vijaytripathi553 <68139428+vijaytripathi553@users.noreply.github.com>
2021-08-05 21:11:27 +0530

    Delete one-to-one directory

--- one-to-one/src/main/java/com/lara/onetoone/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lara.onetoone.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.lara.onetoone.entity.Address;
-
-public interface AddressRepository extends CrudRepository<Address, Integer> {
-
-}
